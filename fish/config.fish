498d8e0e619156488a98c88d96c44ef3fceae489
Gautham Atreyas <89745662+Shogunkayo@users.noreply.github.com>
2023-03-04 15:22:07 +0530

    init

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    	# Commands to run in interactive sessions can go here
+	zoxide init fish | source
+	alias ctf 'cd ~/Code/CTF'
+	alias pes 'cd ~/PES'
+	alias daa 'cd ~/PES/Sem4/DAA'
+	alias la 'cd ~/PES/Sem4/LA'
+	alias os 'cd ~/PES/Sem4/OS'
+	alias mpca 'cd ~/PES/Sem4/MPCA'
+	alias cn 'cd ~/PES/Sem4/CN'
+	alias notes 'cd ~/PES/PES_Notes'	
+	theme_gruvbox dark medium    
+    set -g fish_greeting
+end
+
+
+
